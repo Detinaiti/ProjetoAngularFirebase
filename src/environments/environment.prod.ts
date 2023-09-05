d34fd9c11550caf37358018c9fa6b15c1d1f4799
Detinaiti <giovanicrispim2@gmail.com>
2023-09-05 17:39:56 -0300

    com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyAK2V1BSfl7BI6LiV0ei-YeyyWK_zAnwUA",
-    authDomain: "projetoexemplo-e640c.firebaseapp.com",
-    projectId: "projetoexemplo-e640c",
-    storageBucket: "projetoexemplo-e640c.appspot.com",
-    messagingSenderId: "782096819711",
-    appId: "1:782096819711:web:f72b6236e92b24219973ca",
-    measurementId: "G-520YLH2FX3"
+    apiKey: "AIzaSyDCTsQ6xOVYh86N2mcI1WixFH9BKhScxw4",
+  authDomain: "projetoexemplo-c38d0.firebaseapp.com",
+  projectId: "projetoexemplo-c38d0",
+  storageBucket: "projetoexemplo-c38d0.appspot.com",
+  messagingSenderId: "1019633227659",
+  appId: "1:1019633227659:web:ce6e17b855034b10dc418e",
+  measurementId: "G-08W7G88LL0"
   },
   production: true
 };
